8d65f79053cdae5557d08ca87b547cdcb55506c9
Max Buckmire-Monro <maxbuckmiremonro@gmail.com>
2025-12-03 08:44:10 +0000

    day 2, but smart

--- src/main/scala/day02/day02.scala
+++ src/main/scala/day02/day02.scala
@@ -2,6 +2,7 @@ package day02
 
 import scala.util.boundary
 import scala.util.boundary.break
+import scala.util.matching.Regex
 import util.Input
 
 @main def main(): Unit =
@@ -13,53 +14,27 @@ def parseRange(range: String): scala.collection.immutable.NumericRange.Exclusive
     var splitRange = range.split("-")
     return Range.Long(splitRange(0).toLong, splitRange(1).toLong + 1, 1)
 
-def parseRanges(ranges: String): Array[scala.collection.immutable.NumericRange.Exclusive[Long]] =
-    return ranges.split(",").map(parseRange)
+def parseRanges(ranges: String): List[scala.collection.immutable.NumericRange.Exclusive[Long]] =
+    val rangePattern: Regex = """(\d+)-(\d+)""".r
+    return rangePattern.findAllIn(ranges).toList.map(parseRange)
 
-def isInvalidId(id: Long): Boolean =
-    val idString: String = id.toString()
-    val length: Int = idString.length()
-    if (length % 2 != 0) {
-        return false
-    }
-    val start: String = idString.substring(0, length / 2)
-    val end: String = idString.substring(length / 2)
-    return start == end
+def isInvalidId(id: Long, pattern: Regex): Boolean =
+    return pattern.matches(id.toString())
 
-def isInvalidIdPart2(id: Long): Boolean =
-    val idString: String = id.toString()
-    val length: Int = idString.length()
-    var isInvalid: Boolean = false
-    boundary {
-        for (l <- Range(1, length / 2 + 1)) {
-            boundary {
-                if (length % l != 0) {
-                    boundary.break()
-                }
-            }
-            var repeatedSubstring: String = idString.substring(0, l) * (length / l)
-            if (repeatedSubstring == idString) {
-                isInvalid = true
-                boundary.break()
-            }
-        }
-    }
-    return isInvalid
-
-def part_1(ranges: Array[scala.collection.immutable.NumericRange.Exclusive[Long]]): Long =
+def part_1(ranges: List[scala.collection.immutable.NumericRange.Exclusive[Long]]): Long =
     var invalidIds: Seq[Long] = List()
     for (r <- ranges) {
         for (n <- r) {
-            if isInvalidId(n) then invalidIds :+= n
+            if isInvalidId(n, """^(\d+)\1$""".r) then invalidIds :+= n
         }
     }
     return invalidIds.sum
 
-def part_2(ranges: Array[scala.collection.immutable.NumericRange.Exclusive[Long]]): Long =
+def part_2(ranges: List[scala.collection.immutable.NumericRange.Exclusive[Long]]): Long =
     var invalidIds: Seq[Long] = List()
     for (r <- ranges) {
         for (n <- r) {
-            if isInvalidIdPart2(n) then invalidIds :+= n
+            if isInvalidId(n, """^(\d+)\1+$""".r) then invalidIds :+= n
         }
     }
     return invalidIds.sum
