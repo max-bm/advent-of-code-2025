bf02df617ed4e58769a5328838bd5288b8338d51
Max Buckmire-Monro <maxbuckmiremonro@gmail.com>
2025-12-03 22:50:03 +0000

    day 3, but tidy

--- src/main/scala/day03.scala
+++ src/main/scala/day03.scala
@@ -4,36 +4,27 @@ import util.Input
 
 @main def main(): Unit =
     val joltages: Seq[String] = Input.load("day03")
-    println(part_1(joltages))
-    println(part_2(joltages))
+    val parsedJoltages = joltages.map(x => x.toList.map(_.asDigit))
+    println(part_1(parsedJoltages))
+    println(part_2(parsedJoltages))
 
 def findMaxIndex(joltages: List[Int], offset: Int): Int =
     return joltages.indexOf(joltages.take(joltages.length - offset + 1).max)
 
-def part_1(joltages: Seq[String]): Int =
-    var outputJoltages: Seq[Int] = List()
-    for (j <- joltages) {
-        var output: String = ""
-        var jList: List[Int] = j.toList.map(_.asDigit)
-        for (offset <- Range(2, 0, -1)) {
-            val index: Int = findMaxIndex(jList, offset)
-            output += jList(index).toString()
-            jList = jList.takeRight(jList.length - 1 - index)
-        }
-        outputJoltages = outputJoltages :+ output.toInt
+def findOutputJoltage(bank: List[Int], length: Int): String =
+    var output: String = ""
+    var b: List[Int] = bank
+    for (offset <- Range(length, 0, -1)) {
+        val index: Int = findMaxIndex(b, offset)
+        output += b(index).toString()
+        b = b.takeRight(b.length - 1 - index)
     }
+    return output
+
+def part_1(joltages: Seq[List[Int]]): Int =
+    var outputJoltages: Seq[Int] = for (bank <- joltages) yield findOutputJoltage(bank, 2).toInt
     return outputJoltages.sum
 
-def part_2(joltages: Seq[String]): Long =
-    var outputJoltages: Seq[Long] = List()
-    for (j <- joltages) {
-        var output: String = ""
-        var jList: List[Int] = j.toList.map(_.asDigit)
-        for (offset <- Range(12, 0, -1)) {
-            val index: Int = findMaxIndex(jList, offset)
-            output += jList(index).toString()
-            jList = jList.takeRight(jList.length - 1 - index)
-        }
-        outputJoltages = outputJoltages :+ output.toLong
-    }
+def part_2(joltages: Seq[List[Int]]): Long =
+    var outputJoltages: Seq[Long] = for (bank <- joltages) yield findOutputJoltage(bank, 12).toLong
     return outputJoltages.sum
